c0b9a3d56985f0c43f11377ed1254e138618087d
wlsdksql12 <90670621+wlsdksql12@users.noreply.github.com>
2026-07-10 11:17:38 +0900

    Express의 미들웨어 개념 파악 및 외부 미들웨어(Morgan) 설치

--- src/server.js
+++ src/server.js
@@ -1,14 +1,37 @@
 import express from "express";
-
+import logger from "morgan";
 const PORT = 4000;
 
 const app = express();
 
-app.get("/", (req, res) => {
-  console.log("somebody is trying to go home");
-  return res.send("I still love you");
-  //   return res.end();
-});
+const loggerMiddleware = (req, res, next) => {
+  console.log(`Someone is going to: ${req.url}`);
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1>Not Allowed</h1>");
+  }
+  console.log("Allowed, you may continue.");
+  next();
+};
+
+const handleHome = (req, res) => {
+  return res.send("I love middlewares");
+};
+
+const handleProtected = (req, res) => {
+  return res.send("Welcome to the private lounge.");
+};
+
+// app.use(loggerMiddleware);
+// app.use(privateMiddleware);
+app.use(logger("dev"));
+app.get("/", handleHome);
+app.get("/protected", handleProtected);
 
 app.get("/login", (req, res) => {
   return res.send("Login here.");
