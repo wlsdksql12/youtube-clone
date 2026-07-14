bb4a54d07dcc36d7decb48393da99e8e9e4187d2
wlsdksql12 <90670621+wlsdksql12@users.noreply.github.com>
2026-07-14 22:15:22 +0900

    Router와 Controller 코드 분리 아키텍처 및 URL 파라미터 처리

--- src/server.js
+++ src/server.js
@@ -1,37 +1,47 @@
 import express from "express";
 import logger from "morgan";
+import global from "./routers/globalRouter";
+import user from "./routers/userRouters";
+import video from "./routers/videoRouter";
+
 const PORT = 4000;
 
 const app = express();
 
-const loggerMiddleware = (req, res, next) => {
-  console.log(`Someone is going to: ${req.url}`);
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
+app.use(logger("dev"));
 
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if (url === "/protected") {
-    return res.send("<h1>Not Allowed</h1>");
-  }
-  console.log("Allowed, you may continue.");
-  next();
-};
+app.use("/", global);
+app.use("/video", video);
+app.use("/user", user);
 
-const handleHome = (req, res) => {
-  return res.send("I love middlewares");
-};
+// const loggerMiddleware = (req, res, next) => {
+//   console.log(`Someone is going to: ${req.url}`);
+//   console.log(`${req.method} ${req.url}`);
+//   next();
+// };
 
-const handleProtected = (req, res) => {
-  return res.send("Welcome to the private lounge.");
-};
+// const privateMiddleware = (req, res, next) => {
+//   const url = req.url;
+//   if (url === "/protected") {
+//     return res.send("<h1>Not Allowed</h1>");
+//   }
+//   console.log("Allowed, you may continue.");
+//   next();
+// };
+
+// const handleHome = (req, res) => {
+//   return res.send("I love middlewares");
+// };
+
+// const handleProtected = (req, res) => {
+//   return res.send("Welcome to the private lounge.");
+// };
 
 // app.use(loggerMiddleware);
 // app.use(privateMiddleware);
-app.use(logger("dev"));
-app.get("/", handleHome);
-app.get("/protected", handleProtected);
+// app.use(logger("dev"));
+// app.get("/", handleHome);
+// app.get("/protected", handleProtected);
 
 app.get("/login", (req, res) => {
   return res.send("Login here.");
