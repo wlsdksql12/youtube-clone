98e28993aef94dbfc9876a3b8addadce879db3bf
wlsdksql12 <90670621+wlsdksql12@users.noreply.github.com>
2026-07-23 14:59:12 +0900

    사용자 입력 폼(Form) 데이터를 받아 MongoDB에 영구 저장 및 유효성 검증

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -8,10 +8,13 @@ export const home = async (req, res) => {
     return res.render("server-error");
   }
 };
-export const watch = (req, res) => {
+export const watch = async (req, res) => {
   const id = req.params.id;
+  const video = await Video.findById(id);
+  console.log(video.meta.views);
   return res.render("watch", {
-    pageTitle: `Watching`,
+    pageTitle: video.title,
+    video,
   });
 };
 export const getEdit = (req, res) => {
@@ -32,9 +35,39 @@ export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
 
-export const postUpload = (req, res) => {
+export const postUpload = async (req, res) => {
   //이곳에서 비디오를 videos array에 추가할 예정
-  const title = req.body.title;
+  const { title, description, hashtags } = req.body;
 
-  return res.redirect("/");
+  // database에 저장할 두가지 방법
+  // 1번째
+  try {
+    await Video.create({
+      title,
+      description,
+      createdAt: Date.now(),
+      hashtags: hashtags.split(" ").map((word) => `#${word}`),
+    });
+
+    //   2번째
+    //
+    //   const video = new Video({
+    //     title,
+    //     description,
+    //     createdAt: Date.now(),
+    //     hashtags: hashtags.split(" ").map((word) => `#${word}`),
+    //     meta: {
+    //       views: 0,
+    //       rating: 0,
+    //     },
+    //   });
+    //   await video.save();
+    return res.redirect("/");
+  } catch (error) {
+    console.log(error);
+    return res.render("upload", {
+      pageTitle: "Upload Video",
+      error_Message: error._Message,
+    });
+  }
 };
