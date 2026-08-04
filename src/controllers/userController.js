93621e3042527e7db8831d999616eb82c874b53f
wlsdksql12 <90670621+wlsdksql12@users.noreply.github.com>
2026-08-04 17:13:12 +0900

    세션 로그인 메커니즘을 구현하고 상태에 따라 상단 메뉴 다르게 보여주기

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -60,6 +60,8 @@ export const postLogin = async (req, res) => {
       errorMessage: "Wrong password.",
     });
   }
+  req.session.loggedIn = true;
+  req.session.user = user;
   res.redirect("/");
 };
 
--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 import express from "express";
 import logger from "morgan";
+import session from "express-session";
 import root from "./routers/rootRouter";
 import user from "./routers/userRouters";
 import video from "./routers/videoRouter";
+import { localsMiddleware } from "./middlewares";
 
 const PORT = 4000;
 
@@ -13,6 +15,16 @@ app.use(logger("dev"));
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: "Hello!",
+    resave: true,
+    saveUninitialized: true,
+  }),
+);
+
+app.use(localsMiddleware);
 app.use("/", root);
 app.use("/video", video);
 app.use("/user", user);
